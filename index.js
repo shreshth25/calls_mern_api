87ec129f1e2a703ed620efb53c9c9445cf42b5c1
Shreshth Bansal <shreshthbansal2505@gmail.com>
2023-12-22 16:48:55 +0530

    code update for vercel

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/downloads', express.static('downloads'))
 app.use("/api/", router)
+app.use("/cron", check_details)
 
 app.listen(PORT, ()=>{
     console.log( `Listing to the port ${PORT}`)
